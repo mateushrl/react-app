5f93d96ab10c424b36351246ee49f22e06df194c
Mateus Ramalho <mateushrl918@gmail.com>
2025-07-27 20:47:12 -0300

    Updates

---
+++ _next/static/chunks/app/layout-b813423c08b9400f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3825:function(n,e,_){Promise.resolve().then(_.t.bind(_,2926,23))},2926:function(n){n.exports={style:{fontFamily:"'__Inter_c9dcd7', '__Inter_Fallback_c9dcd7'",fontStyle:"normal"},className:"__className_c9dcd7"}}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3825)}),_N_E=n.O()}]);
